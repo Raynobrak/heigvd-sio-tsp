faa20ccb3a754db02543989887a57bfcb31b105e
RemiAncay <remi.ancay@heig-vd.ch>
2024-11-01 14:38:14 +0100

    get result in a md file

---
+++ result.md
@@ -0,0 +1,60 @@
+# Résultats des heuristiques
+
+## Performance des heuristiques
+
+### pcb442
+
+| Méthode             | Min   | Max   | Moyenne | Optimal | Écart relatif (%) | Écart-type      |
+|---------------------|-------|-------|---------|---------|-------------------|-----------------|
+| RandomTourBuilder   | 56963 | 59646 | 58296   | 50778   | 12.18             | 513.41          |
+| NearestInsertion    | 59212 | 61829 | 60413   | 50778   | 16.61             | 557.92          |
+| FurthestInsertion   | 55428 | 60254 | 57401   | 50778   | 9.16              | 722.09          |
+
+### att532
+
+| Méthode             | Min   | Max   | Moyenne | Optimal | Écart relatif (%) | Écart-type      |
+|---------------------|-------|-------|---------|---------|-------------------|-----------------|
+| RandomTourBuilder   | 94481 | 98438 | 96551   | 86729   | 8.94              | 758.25          |
+| NearestInsertion    | 106471| 108741| 107475  | 86729   | 22.76             | 420.04          |
+| FurthestInsertion   | 91479 | 97886 | 94515   | 86729   | 5.48              | 1084.93         |
+
+### u574
+
+| Méthode             | Min   | Max   | Moyenne | Optimal | Écart relatif (%) | Écart-type      |
+|---------------------|-------|-------|---------|---------|-------------------|-----------------|
+| RandomTourBuilder   | 40665 | 42118 | 41352   | 36905   | 10.19             | 351.20          |
+| NearestInsertion    | 44731 | 46160 | 45530   | 36905   | 21.21             | 304.21          |
+| FurthestInsertion   | 39379 | 42020 | 40805   | 36905   | 6.70              | 460.63          |
+
+### pcb1173
+
+| Méthode             | Min   | Max   | Moyenne | Optimal | Écart relatif (%) | Écart-type      |
+|---------------------|-------|-------|---------|---------|-------------------|-----------------|
+| RandomTourBuilder   | 65132 | 67466 | 66101   | 56892   | 14.48             | 350.13          |
+| NearestInsertion    | 70768 | 72816 | 72223   | 56892   | 24.39             | 441.62          |
+| FurthestInsertion   | 63557 | 67153 | 65706   | 56892   | 11.72             | 496.28          |
+
+### nrw1379
+
+| Méthode             | Min   | Max   | Moyenne | Optimal | Écart relatif (%) | Écart-type      |
+|---------------------|-------|-------|---------|---------|-------------------|-----------------|
+| RandomTourBuilder   | 62727 | 64323 | 63571   | 56638   | 10.75             | 265.64          |
+| NearestInsertion    | 68982 | 69778 | 69461   | 56638   | 21.79             | 144.36          |
+| FurthestInsertion   | 61606 | 64191 | 62875   | 56638   | 8.77              | 396.37          |
+
+### u1817
+
+| Méthode             | Min   | Max   | Moyenne | Optimal | Écart relatif (%) | Écart-type      |
+|---------------------|-------|-------|---------|---------|-------------------|-----------------|
+| RandomTourBuilder   | 67671 | 69614 | 68646   | 57201   | 18.30             | 369.33          |
+| NearestInsertion    | 70139 | 71537 | 70910   | 57201   | 22.62             | 238.50          |
+| FurthestInsertion   | 67153 | 71364 | 69009   | 57201   | 17.40             | 550.54          |
+
+## Conclusion
+
+On peut voir que sur tout les sets de données, la méthode de construction de tour par l'heristique **« Insertion la plus éloignée »** est la plus performante. Elle a l'écart relatif le plus faible et donc ses résultats sont les plus proches de l'optimal.
+Nous avons ensuite la méthode de construction de tour par l'heristique **« Insertion la plus proche »** qui est la moins performante et finalement la méthode de construction de tour **aléatoire** qui se situe juste après la méthode de l'insertion la plus éloignée.
+
+Globallement, les résultats sont très proches de l'optimal, ce qui est très satisfaisant.
+
+On note aussi que bien que la méthode de l'insertion la plus éloignée soit la plus performante, elle est aussi celle avec le plus grand écart-type. Aussi, toutes les méthodes ont un temps d'exécution très similaire, ce qui est très intéressant.
--- src/main/java/ch/heig/sio/lab1/groupH/Analyze.java
+++ src/main/java/ch/heig/sio/lab1/groupH/Analyze.java
@@ -2,75 +2,81 @@ package ch.heig.sio.lab1.groupH;
 
 import java.io.FileNotFoundException;
 
+import ch.heig.sio.lab1.display.ObservableTspConstructiveHeuristic;
 import ch.heig.sio.lab1.tsp.TspData;
 import ch.heig.sio.lab1.tsp.TspParsingException;
 
 public final class Analyze {
-  public static final long RNG_SEED = 0x134DA73;
+  public static final long RNG_SEED = 0x134DA73; // Graine pour le générateur de nombres aléatoires
 
-  public static void main(String[] args) throws TspParsingException, FileNotFoundException {
-    // TODO
-    //  - Renommer le package ;
-    //  - Implémenter les différentes heuristiques en écrivant le code dans ce package, et uniquement celui-ci
-    //    (sous-packages et packages de tests ok) ;
-    //  - Factoriser le code commun entre les différentes heuristiques ;
-    //  - Documentation soignée comprenant :
-    //    - la javadoc, avec auteurs et description des implémentations ;
-    //    - des commentaires sur les différentes parties de vos algorithmes.
+  /// Analyse les différentes heuristiques de construction de tournée pour différents jeux de données
+  /// Affiche certaines statistiques sur les tournées générées
+  /// @author Group H - Ancay Rémi, Charbonnier Lucas
+  public static void main(String[] args) throws TspParsingException, FileNotFoundException {   
+    long optimales[] = {50778, 86729, 36905, 56892, 56638, 57201}; // Longueurs optimales des tournées
+    String[] datas = {"pcb442", "att532", "u574", "pcb1173", "nrw1379", "u1817"}; // Jeux de données
+    String[] builderNames = {"RandomTourBuilder", "NearestInsertion", "FurthestInsertion"}; // Constructeurs de tournée
 
-    // Longueurs optimales :
-    // pcb442 : 50778
-    // att532 : 86729
-    // u574 : 36905
-    // pcb1173   : 56892
-    // nrw1379  : 56638
-    // u1817 : 57201
-
-    // Exemple de lecture d'un jeu de données :
-    //TspData data = TspData.fromFile("data/att532.dat");
+    // Pour chaque jeu de données
+    for (int j = 0; j < datas.length; j++) {
+        TspData data = TspData.fromFile("data/" + datas[j] + ".dat");
+        int N = data.getNumberOfCities(); // Nombre de villes dans le jeu de données
     
-     long optimales[] = {50778, 86729, 36905, 56892, 56638, 57201};
-     String[] datas = {"pcb442", "att532", "u574", "pcb1173", "nrw1379", "u1817"};
+        // Boucle sur les différents constructeurs de tournée
+        for (String builderName : builderNames) {
+            long minLength = Integer.MAX_VALUE; // Longueur minimale
+            long maxLength = Integer.MIN_VALUE; // Longueur maximale
+            long meanLength = 0; // Longueur moyenne
+            long[] lengths = new long[N]; // Stocke les longueurs de chaque tournée pour calculer l'écart-type
+    
+            // Sélection du constructeur de tournée
+            ObservableTspConstructiveHeuristic builder;
+            if (builderName.equals("RandomTourBuilder")) {
+                builder = new RandomTourBuilder();
+            } else if (builderName.equals("NearestInsertion")) {
+                builder = new DistanceBasedTourBuilder(false);
+            } else {
+                builder = new DistanceBasedTourBuilder(true);
+            }
+    
+            // Calcul de la longueur minimale, maximale et moyenne pour chaque ville de départ
+            for (int i = 0; i < N; i++) {
+                long length = builder.computeTour(data, i).length();
+                lengths[i] = length;
 
-     // Pour chaque jeu de données
-     for (int j = 0; j < datas.length; j++) {
-      TspData data = TspData.fromFile("data/" + datas[j] + ".dat");
-
-      long minLength = Integer.MAX_VALUE;
-      long maxLength = Integer.MIN_VALUE;
-      long meanLength = 0;
-
-      int N = data.getNumberOfCities(); // Nombre de villes
-
-      // Calcul de la longueur minimale, maximale et moyenne de l'heuristic RandomTourBuilder pour chaque ville de départ
-      for (int i = 0; i < N; i++) {
-        RandomTourBuilder randomTourBuilder = new RandomTourBuilder();
-        long length = randomTourBuilder.computeTour(data, i).length();
-
-        meanLength += length;
-        if (length < minLength) {
-          minLength = length;
+                meanLength += length;
+    
+                if (length < minLength)
+                  minLength = length;
+    
+                if (length > maxLength) 
+                  maxLength = length;
+            }
+    
+            // Calcul de la longueur moyenne
+            meanLength /= N;
+    
+            // Calcul de l'écart-type
+            double sumSquaredDifferences = 0.0;
+            for (int i = 0; i < N; i++) {
+                sumSquaredDifferences += Math.pow(lengths[i] - meanLength, 2);
+            }
+            double variance = sumSquaredDifferences / N;
+            double sd = Math.sqrt(variance); // Ecart-type
+    
+            // Calcul de l'écart relatif
+            double relativeMin = ((minLength - optimales[j]) / (double) optimales[j]) * 100;
+    
+            // Affichage des résultats
+            System.out.println(builderName + ": " + datas[j]);
+            System.out.println("Min: " + minLength);
+            System.out.println("Max: " + maxLength);
+            System.out.println("Mean: " + meanLength);
+            System.out.println("Optimal: " + optimales[j]);
+            System.out.println("Ecart relatif: " + relativeMin + "%");
+            System.out.println("Ecart-type: " + sd);
+            System.out.println();
         }
-
-        if (length > maxLength) {
-          maxLength = length;
-        }
-      }
-
-      meanLength /= N;
-
-      // Statistiques
-      double relativeMin = ((minLength - optimales[j]) / (double) optimales[j]) * 100;
-
-      // Affichage des résultats
-      System.out.println("RandomTourBuilder: " + datas[j]);
-      System.out.println("Min: " + minLength);
-      System.out.println("Max: " + maxLength);
-      System.out.println("Mean: " + meanLength);
-      System.out.println("Optimal: " + optimales[j]);
-      System.out.println("Ecart relatif: " + relativeMin + "%");
-      System.out.println();
-     }
+    }
   }
-
 }
